8ac4e431e7415e53d7b7d409ad69d144fb296aa4
Keeshaul Martinez <keeshaulmartinez@Keeshauls-MacBook-Air-2.local>
2026-03-03 12:45:40 -0700

    Add folder with game files

---
+++ godot_files/menu.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+# Start button
+func _on_button_1_pressed() -> void:
+	get_tree().change_scene_to_file("res://game.tscn")
+
+# Leaderboard button
+func _on_button_2_pressed() -> void: 
+	pass # Replace with function body
+
+#  Exit button 
+func _on_button_3_pressed() -> void: 
+	get_tree().quit()
+	
