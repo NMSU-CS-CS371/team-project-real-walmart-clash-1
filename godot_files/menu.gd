dff78920dcc3ab308d5fa764c8555c6012f6f897
Nathan Molina <nathanmolina@Nathans-MacBook-Pro.local>
2026-03-09 16:04:44 -0600

    Updated menu UI

--- godot_files/menu.gd
+++ godot_files/menu.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Control
 
 
 # Called when the node enters the scene tree for the first time.
@@ -7,19 +7,49 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
 # Start button
-func _on_button_1_pressed() -> void:
+@onready var start_button = $TextureButton1
+
+func _on_texture_button_1_pressed() -> void:
 	get_tree().change_scene_to_file("res://game.tscn")
 
+func _on_texture_button_1_mouse_entered():
+	var tween = create_tween()
+	tween.tween_property(start_button, "scale", Vector2(1.2,1.2), 0.2)
+	
+func _on_texture_button_1_mouse_exited():
+	var tween = create_tween()
+	tween.tween_property(start_button, "scale", Vector2(1,1), 0.2)
+
 # Leaderboard button
-func _on_button_2_pressed() -> void: 
+@onready var leader_board_b = $TextureButton2
+
+func _on_texture_button_2_pressed() -> void: 
 	get_tree().change_scene_to_file("res://leaderboard.tscn")
 
+func _on_texture_button_2_mouse_entered():
+	var tween = create_tween()
+	tween.tween_property(leader_board_b, "scale", Vector2(1.2,1.2), 0.2)
+	
+func _on_texture_button_2_mouse_exited():
+	var tween = create_tween()
+	tween.tween_property(leader_board_b, "scale", Vector2(1,1), 0.2)
+
 #  Exit button 
-func _on_button_3_pressed() -> void: 
+@onready var exit_button = $TextureButton3
+
+func _on_texture_button_3_pressed() -> void: 
 	get_tree().quit()
+
+func _on_texture_button_3_mouse_entered():
+	var tween = create_tween()
+	tween.tween_property(exit_button, "scale", Vector2(1.2,1.2), 0.2)
+	
+func _on_texture_button_3_mouse_exited():
+	var tween = create_tween()
+	tween.tween_property(exit_button, "scale", Vector2(1,1), 0.2)
 	
