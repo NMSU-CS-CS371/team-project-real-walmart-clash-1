9f9c160cb9680fb310915b2c313710e77de1cebd
Keeshaul Martinez <keemartinez01@gmail.com>
2026-03-28 23:18:34 -0600

    Fixed null issue

--- godot_files/GDScript/crate.gd
+++ godot_files/GDScript/crate.gd
@@ -1,29 +1,25 @@
 extends Area2D
 @export var crate_texture: Texture2D
 
-# Variables 
 var player_in_area = false
 
 func _ready():
 	$"../Label".visible = false
 
-# Interaction 
 func _process(delta):
 	if player_in_area and Input.is_action_just_pressed("Interact"):
 		interact()
 
-# Body enter crate area 
 func _on_Area2D_body_entered(body):
 	if body.name == "Player":
 		player_in_area = true
 		$Label.visible = true
 
-# Body leaves crate area 
 func _on_Area2D_body_exited(body):
 	if body.name == "Player":
 		player_in_area = false
 		$Label.visible = false
 
-# Function for if "E" is pressed 
+# Interact function 
 func interact():
-	print("Interacted with crate!") # Check if working 
+	print("Interacted with crate!")
