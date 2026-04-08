389352c281cb6945ef837f9a71db6a371c9ea81d
Eduardo Arevalo <eddyarevalo23@gmail.com>
2026-04-07 21:27:52 -0600

    added turret script

---
+++ godot_files/turret.gd
@@ -0,0 +1,62 @@
+extends Node2D
+
+var enemies_in_range = []
+var last_direction = ""
+
+func _ready():
+	add_to_group("tower")
+
+func _process(delta):
+	if enemies_in_range.size() > 0:
+		var target = get_closest_enemy()
+		if target:
+			update_direction_animation(target.global_position)
+
+func get_closest_enemy():
+	var closest = null
+	var closest_dist = INF
+
+	for e in enemies_in_range:
+		if not is_instance_valid(e):
+			continue
+
+		var dist = global_position.distance_to(e.global_position)
+		if dist < closest_dist:
+			closest_dist = dist
+			closest = e
+
+	return closest
+
+func update_direction_animation(target_pos: Vector2):
+	var direction = (target_pos - global_position).normalized()
+	var angle = rad_to_deg(direction.angle())
+	var anim = "Robot_idle_S"
+
+	if angle >= -22.5 and angle < 22.5:
+		anim = "Robot_Idle_E"
+	elif angle >= 22.5 and angle < 67.5:
+		anim = "Robot_idle_SE"
+	elif angle >= 67.5 and angle < 112.5:
+		anim = "Robot_idle_S"
+	elif angle >= 112.5 and angle < 157.5:
+		anim = "Robot_idle_WS"
+	elif angle >= 157.5 or angle < -157.5:
+		anim = "Robot_idle_W"
+	elif angle >= -157.5 and angle < -112.5:
+		anim = "Robot_idle_NW"
+	elif angle >= -112.5 and angle < -67.5:
+		anim = "Robot_idle_N"
+	elif angle >= -67.5 and angle < -22.5:
+		anim = "Robot_idle_NE"
+
+	if anim != last_direction:
+		last_direction = anim
+		$AnimatedSprite2D.play(anim)
+
+func _on_area_2d_detection_body_entered(body):
+	if body.is_in_group("enemy"):
+		enemies_in_range.append(body)
+
+func _on_area_2d_detection_body_exited(body):
+	if body.is_in_group("enemy"):
+		enemies_in_range.erase(body)
